93abdc21370d88ea9ceda8ce84bf81e8eb40ff36
jonathan laporte <laportej17@gmail.com>
2026-03-28 19:26:16 -0400

    external database hossted

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, SQLModel, create_engine
 
-DATABASE_URL = 'sqlite:///./openspot.db'
-engine = create_engine(DATABASE_URL, echo=False, connect_args={'check_same_thread': False})
+DATABASE_URL = 'postgresql://neondb_owner:npg_H0uYLZDPFr2m@ep-mute-flower-anxv13jr-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+engine = create_engine(DATABASE_URL, echo=False)
 
 
 def create_db_and_tables() -> None:
